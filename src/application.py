00f79cf60565579676412646f02f9f6946a7c9f2
thomasZhang98 <elijah_zhang@outlook.com>
2022-10-19 23:29:25 -0400

    Added Users microservice

---
+++ src/application.py
@@ -0,0 +1,40 @@
+from flask import Flask, Response, request
+import json
+from user_resource import UserResource
+from flask_cors import CORS
+
+# Create the Flask application object.
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route("/api/users", methods=["GET"])
+def get_users():
+
+    users = UserResource.get_all()
+
+    if users:
+        rsp = Response(json.dumps(users), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+
+@app.route("/api/users/<uid>", methods=["GET"])
+def get_user_by_uid(uid):
+
+    result = UserResource.get_by_key(uid)
+
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5011)
+
---
+++ src/user_resource.py
@@ -0,0 +1,46 @@
+import os
+import pymysql
+
+database = 'f22_databases'
+table = 'users'
+
+
+class UserResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+        user = os.environ.get("DBUSER")
+        password = os.environ.get("DBPW")
+        host = os.environ.get("DBHOST")
+
+        conn = pymysql.connect(
+            user=user,
+            password=password,
+            host=host,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+        sql = f"SELECT * FROM {database}.{table} where uid=%s"
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        cur.execute(sql, args=key)
+        user = cur.fetchone()
+
+        return user
+
+    @staticmethod
+    def get_all():
+        sql = f"SELECT * FROM {database}.{table}"
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        cur.execute(sql)
+        users = cur.fetchall()
+
+        return users
